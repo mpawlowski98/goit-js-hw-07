2a3de608d92c2872754f7e9b1c680abd900cfd96
Michał Pawłowski <107926477+mpawlowski98@users.noreply.github.com>
2022-12-18 21:15:16 +0100

    Up 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const myGallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const myGalleryItemsMap = galleryItems
+  .map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>`;
+  })
+  .join("");
+myGallery.insertAdjacentHTML("beforeend", myGalleryItemsMap);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+  captionPosition: `bottom`,
+});
